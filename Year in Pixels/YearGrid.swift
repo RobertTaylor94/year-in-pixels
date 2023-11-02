03470d1af59ed28313835a6b39bb70685fe9a58e
Robert Taylor <rjt903@gmail.com>
2023-11-02 18:41:38 +0000

    Added cube View that allows color to be passed for fill

---
+++ Year in Pixels/YearGrid.swift
@@ -0,0 +1,18 @@
+//
+//  YearGrid.swift
+//  Year in Pixels
+//
+//  Created by Robert Taylor on 02/11/2023.
+//
+
+import SwiftUI
+
+struct YearGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    YearGrid()
+}
