c193a5af7afc4d36be246d1601b9fe99530170e6
Robert Taylor <rjt903@gmail.com>
2023-11-02 17:31:36 +0000

    Made floating add button

---
+++ Year in Pixels/FloatingButton.swift
@@ -0,0 +1,48 @@
+//
+//  FloatingButton.swift
+//  Year in Pixels
+//
+//  Created by Robert Taylor on 31/10/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct FloatingButton: View {
+    let icon: String
+    
+    @State var isPresented: Bool = false
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            HStack{
+                Spacer()
+                Button(action: {
+                    isPresented.toggle()
+                }){
+                    Image(systemName: icon)
+                        .font(.system(size: 25))
+                        .foregroundColor(.white)
+                }
+                .frame(width: 80, height: 80)
+                .background(Color.red)
+                .cornerRadius(50)
+                .shadow(radius: 20)
+                .sheet(isPresented: $isPresented, onDismiss: didDismiss) {
+                    AddDay()
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    func didDismiss() {
+        
+    }
+    
+}
+
+#Preview {
+    FloatingButton(icon: "plus", isPresented: true)
+}
