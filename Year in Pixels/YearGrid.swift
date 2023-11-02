23e245fc94698a41d51faba1b33205fd57cc744e
Robert Taylor <rjt903@gmail.com>
2023-11-02 18:42:13 +0000

    Created Year Grid with correct days and heading

--- Year in Pixels/YearGrid.swift
+++ Year in Pixels/YearGrid.swift
@@ -9,7 +9,37 @@ import SwiftUI
 
 struct YearGrid: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top, spacing: 1){
+            YearColumn(days: 31, month: "J")
+            YearColumn(days: 28, month: "F")
+            YearColumn(days: 31, month: "M")
+            YearColumn(days: 30, month: "A")
+            YearColumn(days: 31, month: "M")
+            YearColumn(days: 30, month: "J")
+            YearColumn(days: 31, month: "J")
+            YearColumn(days: 31, month: "A")
+            YearColumn(days: 30, month: "S")
+            YearColumn(days: 31, month: "O")
+            YearColumn(days: 30, month: "N")
+            YearColumn(days: 31, month: "D")
+        }
+    }
+}
+
+struct YearColumn: View {
+    
+    @State var days: Int
+    @State var month: String
+    
+    var body: some View {
+        
+        VStack(spacing: 1){
+            Text(month)
+                .padding(1)
+            ForEach(0..<days, id: \.self){_ in
+                DayCube()
+            }
+        }
     }
 }
 
